2911fe9e0b13c493ddd7b115b6433ff9990fe401
insafsen <insaf.sendide@usmba.ac.ma>
2026-08-05 20:02:26 +0100

    Modification de config.py

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
-# ===========================
-# CONFIGURATION RAILWAY MYSQL
-# ===========================
+import os
 
-HOST = "gondola.proxy.rlwy.net"
-PORT = 22685
-
-USER = "root"
-PASSWORD = "NLXbhGIjjURLQZkYwpFcSnATgyhijweT"
-DATABASE = "railway"
+HOST = os.environ.get("HOST")
+USER = os.environ.get("USER")
+PASSWORD = os.environ.get("PASSWORD")
+DATABASE = os.environ.get("DATABASE")
+PORT = int(os.environ.get("PORT"))
