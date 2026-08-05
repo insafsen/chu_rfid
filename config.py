cfc408185e4551e38ec97dcf96fe0aa917599adf
insafsen <insaf.sendide@usmba.ac.ma>
2026-08-05 23:56:26 +0100

    Debug config

--- config.py
+++ config.py
@@ -1,5 +1,10 @@
 import os
 
+print("MYSQLHOST =", os.environ.get("MYSQLHOST"))
+print("MYSQLPORT =", os.environ.get("MYSQLPORT"))
+print("MYSQLUSER =", os.environ.get("MYSQLUSER"))
+print("MYSQLDATABASE =", os.environ.get("MYSQLDATABASE"))
+
 HOST = os.environ.get("MYSQLHOST")
 USER = os.environ.get("MYSQLUSER")
 PASSWORD = os.environ.get("MYSQLPASSWORD")
